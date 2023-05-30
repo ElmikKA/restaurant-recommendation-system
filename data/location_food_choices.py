11b6797e63b431eecbd2be50e1e9f84518469b17
elmik <elmik.karl@gmail.com>
2023-05-30 16:47:25 +0300

    file change, made all choices to dictionarys

--- data/location_food_choices.py
+++ data/location_food_choices.py
@@ -1,11 +1,10 @@
 
 
 location_food_choices = {
-    "Tallinn": { "a": "German", "b": "Japanese", "c": "Vegetarian", "d": "French", "f" :"African", "g": "American", "h": "Barbecue", "i": "Czech", "j": "Chinese", "k": "Thai",
-                    "l": "Mexican", "m": "Indian", "n": "Cafe", "o": "Pizza", "p": "Italian"},
-    "Tartu": set(["germa", "japanese", "vegetarian", "american", "barbecue", "czech", "chinese", "thai",
-                    "mexican", "cafe", "pizza", "italian"]),
-    "Narva": set(["vegetarian", "french", "african", "barbecue", "czech", "chinese", "thai",
-                    "mexican", "cafe", "pizza"])
-    
+    "Tallinn":  { "a": "German", "b": "Japanese", "c": "Vegetarian", "d": "French", "f" :"African", "g": "American", "h": "Barbecue",
+                    "i": "Czech", "j": "Chinese", "k": "Thai", "l": "Mexican", "m": "Indian", "n": "Cafe", "o": "Pizza", "p": "Italian"},
+    "Tartu":    {"a": "German", "b": "Japanese", "c": "Vegetarian", "d": "American", "f": "Barbecue", "g": "Czech", "h": "Chinese", "i": "Thai",
+                    "j": "Mexican", "k": "Cafe", "l": "Pizza", "m": "Italian"},
+    "Narva":    {"a": "Vegetarian", "b": "French", "c": "African", "d": "Barbecue", "f": "Czech", "g": "Chinese", "h": "Thai",
+                    "i": "Mexican", "j": "Cafe","k": "Pizza"}
 }
