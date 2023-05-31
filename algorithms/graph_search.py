42de552560f62489cc0941f999724fbdaa392f43
elmik <elmik.karl@gmail.com>
2023-05-31 14:28:17 +0300

    updated breath first search

--- algorithms/graph_search.py
+++ algorithms/graph_search.py
@@ -1,21 +1,21 @@
 
-def breath_first_search(graph, start_vertex, target_value): 
+from collections import deque
+
+
+def breath_first_search(graph, start_vertex, target_value):
     path = [start_vertex]
-    vertex_and_path = [start_vertex, path]
-    bfs_queue = [vertex_and_path]
+    bfs_queue = deque([(start_vertex, path)])
     visited = set()
 
-    while bfs_queue: 
-
-        current_vertex, path = bfs_queue.pop(0)
+    while bfs_queue:
+        current_vertex, path = bfs_queue.popleft()
         visited.add(current_vertex)
 
+        if current_vertex == target_value:
+            return path
+
         for neighbor in graph[current_vertex]:
-
             if neighbor not in visited:
+                bfs_queue.append((neighbor, path + [neighbor]))
 
-                if neighbor == target_value:
-                    return path + [neighbor]
-                
-                else: 
-                    bfs_queue.append([neighbor, path + [neighbor]])
+    return None
