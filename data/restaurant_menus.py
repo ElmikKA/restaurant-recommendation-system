56f4ebeabd47df50d4f7e9d1ea859dccf96a7cf3
elmik <elmik.karl@gmail.com>
2023-06-01 20:04:36 +0300

    updated restaurant_menus

--- data/restaurant_menus.py
+++ data/restaurant_menus.py
@@ -107,5 +107,443 @@ restaurant_menus = {
             "Cheesecake": 8.99,
             "Ice Cream Sundae": 5.99
         }
+    }, 
+
+    "Vegetarian": {
+        "Appetizers": {
+            "Vegetable Spring Rolls": 6.99,
+            "Stuffed Mushrooms": 7.99,
+            "Bruschetta": 8.99,
+            "Sweet Potato Fries": 5.99
+        },
+        "Burgers": {
+            "Black Bean Burger": 10.99,
+            "Portobello Mushroom Burger": 11.99,
+            "Quinoa Burger": 12.99,
+            "Chickpea Burger": 12.99
+        },
+        "Sandwiches": {
+            "Caprese Sandwich": 9.99,
+            "Grilled Veggie Sandwich": 8.99,
+            "Eggplant Parmesan Sandwich": 10.99,
+            "Hummus and Veggie Wrap": 11.99
+        },
+        "Salads": {
+            "Greek Salad": 9.99,
+            "Mediterranean Salad": 10.99,
+            "Spinach and Strawberry Salad": 8.99,
+            "Quinoa Salad": 11.99
+        },
+        "Pasta": {
+            "Penne Arrabbiata": 12.99,
+            "Vegetable Lasagna": 13.99,
+            "Pesto Linguine": 11.99,
+            "Mushroom Risotto": 14.99
+        },
+        "Desserts": {
+            "Fruit Salad": 6.99,
+            "Vegan Chocolate Cake": 7.99,
+            "Coconut Milk Ice Cream": 8.99,
+            "Chia Pudding": 5.99
+        }
+    },
+
+    "French": {
+        "Appetizers": {
+            "Escargots": 12.99,
+            "French Onion Soup": 8.99,
+            "Foie Gras": 18.99,
+            "Cheese Plate": 14.99
+        },
+        "Entrees": {
+            "Coq au Vin": 22.99,
+            "Bouillabaisse": 26.99,
+            "Beef Bourguignon": 24.99,
+            "Ratatouille": 18.99
+        },
+        "Fish": {
+            "Sole Meuniere": 28.99,
+            "Trout Amandine": 25.99,
+            "Salmon en Papillote": 23.99,
+            "Mussels Mariniere": 19.99
+        },
+        "Poultry": {
+            "Duck Confit": 27.99,
+            "Chicken Cordon Bleu": 21.99,
+            "Quail with Grapes": 29.99,
+            "Turkey Roulade": 23.99
+        },
+        "Desserts": {
+            "Crème Brûlée": 9.99,
+            "Tarte Tatin": 10.99,
+            "Profiteroles": 11.99,
+            "Mille-Feuille": 12.99
+        }
+    },
+
+    "African": {
+        "Appetizers": {
+            "Samosas": 7.99,
+            "Plantain Chips": 5.99,
+            "Maafe (Peanut Soup)": 8.99,
+            "Akara (Bean Fritters)": 6.99
+        },
+        "Main Courses": {
+            "Jollof Rice": 15.99,
+            "Bobotie": 17.99,
+            "Tagine": 18.99,
+            "Suya (Grilled Skewers)": 14.99
+        },
+        "Vegetarian": {
+            "Vegetable Couscous": 13.99,
+            "Yam and Vegetable Stew": 12.99,
+            "Mushroom Pilau": 14.99,
+            "Ful Medames": 11.99
+        },
+        "Seafood": {
+            "Grilled Tilapia": 19.99,
+            "Shrimp Yassa": 21.99,
+            "Calamari Tagine": 20.99,
+            "Fish Stew": 18.99
+        },
+        "Desserts": {
+            "Baklava": 7.99,
+            "Fruit Salad": 6.99,
+            "Malva Pudding": 8.99,
+            "Papaya Ice Cream": 5.99
+        }
+    },
+
+     "Barbecue": {
+        "Appetizers": {
+            "Chicken Wings": 8.99,
+            "Pulled Pork Sliders": 7.99,
+            "Fried Pickles": 6.99,
+            "BBQ Nachos": 9.99
+        },
+        "Platters": {
+            "Pork Ribs": 18.99,
+            "Brisket": 17.99,
+            "Pulled Pork": 16.99,
+            "Sausage Platter": 15.99
+        },
+        "Sandwiches": {
+            "BBQ Pulled Chicken": 10.99,
+            "Smoked Turkey": 9.99,
+            "Beef Brisket": 11.99,
+            "Pulled Pork": 10.99
+        },
+        "Sides": {
+            "Coleslaw": 3.99,
+            "Baked Beans": 4.99,
+            "Cornbread": 2.99,
+            "Mac and Cheese": 5.99
+        },
+        "Desserts": {
+            "Pecan Pie": 6.99,
+            "Banana Pudding": 5.99,
+            "Fruit Cobbler": 7.99,
+            "Brownie": 4.99
+        }
+    },
+
+    "Czech": {
+        "Appetizers": {
+            "Fried Cheese": 8.99,
+            "Potato Pancakes": 6.99,
+            "Pickled Sausages": 7.99,
+            "Česneková polévka (Garlic Soup)": 5.99
+        },
+        "Main Courses": {
+            "Marinated Beef": 15.99,
+            "Pork with Dumplings and Sauerkraut": 14.99,
+            "Goulash": 13.99,
+            "Pork Knuckle": 17.99
+        },
+        "Side Dishes": {
+            "Potato Dumplings": 3.99,
+            "French Fries": 4.99,
+            "Fried Potato Pancake": 5.99,
+            "Sauerkraut": 2.99
+        },
+        "Desserts": {
+            "Trdelník": 6.99,
+            "Plum Dumplings": 7.99,
+            "Crepes": 5.99,
+            "Honey Cake": 8.99
+        }
+    },
+
+     "Chinese": {
+        "Appetizers": {
+            "Spring Rolls": 6.99,
+            "Potstickers": 7.99,
+            "Scallion Pancakes": 5.99,
+            "Sesame Chicken Wings": 8.99
+        },
+        "Soup": {
+            "Hot and Sour Soup": 6.99,
+            "Wonton Soup": 5.99,
+            "Egg Drop Soup": 5.99,
+            "Chicken Corn Soup": 6.99
+        },
+        "Main Courses": {
+            "Kung Pao Chicken": 13.99,
+            "General Tso's Chicken": 12.99,
+            "Beef with Broccoli": 14.99,
+            "Sweet and Sour Pork": 11.99
+        },
+        "Noodles": {
+            "Chow Mein": 10.99,
+            "Lo Mein": 11.99,
+            "Dan Dan Noodles": 12.99,
+            "Beef Chow Fun": 13.99
+        },
+        "Rice": {
+            "Fried Rice": 10.99,
+            "Yangzhou Fried Rice": 12.99,
+            "Pineapple Fried Rice": 11.99,
+            "Shrimp Fried Rice": 13.99
+        },
+        "Desserts": {
+            "Mango Pudding": 5.99,
+            "Almond Cookies": 4.99,
+            "Fortune Cookies": 3.99,
+            "Red Bean Soup": 6.99
+        }
+    },
+
+    "Thai": {
+        "Appetizers": {
+            "Satay Skewers": 8.99,
+            "Spring Rolls": 6.99,
+            "Tom Yum Soup": 7.99,
+            "Fish Cakes": 7.99
+        },
+        "Curries": {
+            "Green Curry": 14.99,
+            "Red Curry": 13.99,
+            "Massaman Curry": 15.99,
+            "Panang Curry": 14.99
+        },
+        "Stir-Fries": {
+            "Pad Thai": 12.99,
+            "Pad See Ew": 11.99,
+            "Basil Chicken": 13.99,
+            "Cashew Nut Stir-Fry": 12.99
+        },
+        "Noodles": {
+            "Tom Yum Noodle Soup": 11.99,
+            "Pad Kee Mao": 12.99,
+            "Rad Na": 13.99,
+            "Pad Woon Sen": 12.99
+        },
+        "Rice": {
+            "Pineapple Fried Rice": 12.99,
+            "Thai Fried Rice": 11.99,
+            "Coconut Rice": 3.99,
+            "Jasmine Rice": 2.99
+        },
+        "Desserts": {
+            "Mango Sticky Rice": 7.99,
+            "Thai Tea Ice Cream": 5.99,
+            "Coconut Ice Cream": 6.99,
+            "Banana Fritters": 6.99
+        }
+    },
+
+    "Mexican": {
+        "Appetizers": {
+            "Guacamole and Chips": 8.99,
+            "Queso Fundido": 7.99,
+            "Nachos Supreme": 9.99,
+            "Mexican Street Corn": 6.99
+        },
+        "Tacos": {
+            "Carne Asada Tacos": 12.99,
+            "Carnitas Tacos": 11.99,
+            "Fish Tacos": 12.99,
+            "Vegetable Tacos": 10.99
+        },
+        "Enchiladas": {
+            "Cheese Enchiladas": 10.99,
+            "Chicken Enchiladas": 11.99,
+            "Beef Enchiladas": 12.99,
+            "Spinach Enchiladas": 10.99
+        },
+        "Burritos": {
+            "Beef Burrito": 12.99,
+            "Chicken Burrito": 11.99,
+            "Shrimp Burrito": 13.99,
+            "Bean and Cheese Burrito": 9.99
+        },
+        "Sides": {
+            "Mexican Rice": 3.99,
+            "Refried Beans": 4.99,
+            "Mexican Street Corn": 5.99,
+            "Chips and Salsa": 3.99
+        },
+        "Desserts": {
+            "Churros": 5.99,
+            "Sopapillas": 6.99,
+            "Flan": 6.99,
+            "Tres Leches Cake": 7.99
+        }
+    },
+
+    "Indian": {
+        "Appetizers": {
+            "Samosa": 6.99,
+            "Pakora": 5.99,
+            "Chicken Tikka": 8.99,
+            "Paneer Tikka": 7.99
+        },
+        "Curries": {
+            "Butter Chicken": 14.99,
+            "Chicken Tikka Masala": 13.99,
+            "Palak Paneer": 12.99,
+            "Chana Masala": 11.99
+        },
+        "Biryani": {
+            "Chicken Biryani": 13.99,
+            "Vegetable Biryani": 11.99,
+            "Shrimp Biryani": 15.99,
+            "Lamb Biryani": 14.99
+        },
+        "Naan Bread": {
+            "Plain Naan": 2.99,
+            "Garlic Naan": 3.99,
+            "Butter Naan": 3.99,
+            "Cheese Naan": 4.99
+        },
+        "Rice": {
+            "Basmati Rice": 2.99,
+            "Jeera Rice": 3.99,
+            "Pulao Rice": 4.99,
+            "Lemon Rice": 3.99
+        },
+        "Desserts": {
+            "Gulab Jamun": 5.99,
+            "Kheer": 6.99,
+            "Rasmalai": 6.99,
+            "Jalebi": 5.99
+        }
+    },
+    
+    "Cafe": {
+        "Coffee": {
+            "Espresso": 2.99,
+            "Cappuccino": 3.99,
+            "Latte": 4.99,
+            "Mocha": 4.99
+        },
+        "Tea": {
+            "Green Tea": 2.99,
+            "Earl Grey": 3.99,
+            "Chai Tea": 4.99,
+            "Herbal Tea": 3.99
+        },
+        "Pastries": {
+            "Croissant": 2.99,
+            "Muffin": 2.99,
+            "Scone": 2.99,
+            "Danish": 2.99
+        },
+        "Sandwiches": {
+            "Turkey Sandwich": 7.99,
+            "Chicken Salad Sandwich": 8.99,
+            "Vegetable Panini": 6.99,
+            "Caprese Sandwich": 7.99
+        },
+        "Salads": {
+            "Caesar Salad": 7.99,
+            "Greek Salad": 8.99,
+            "Cobb Salad": 9.99,
+            "Spinach Salad": 7.99
+        },
+        "Desserts": {
+            "Cheesecake": 5.99,
+            "Chocolate Brownie": 4.99,
+            "Carrot Cake": 5.99,
+            "Apple Tart": 4.99
+        }
+    },
+
+    "Pizza": {
+        "Classic Pizzas": {
+            "Margherita": 12.99,
+            "Pepperoni": 13.99,
+            "Hawaiian": 14.99,
+            "Vegetarian": 14.99
+        },
+        "Specialty Pizzas": {
+            "Supreme": 16.99,
+            "BBQ Chicken": 15.99,
+            "Meat Lovers": 17.99,
+            "Mediterranean": 16.99
+        },
+        "Gourmet Pizzas": {
+            "Truffle Mushroom": 18.99,
+            "Prosciutto Arugula": 19.99,
+            "Buffalo Chicken": 17.99,
+            "Four Cheese": 18.99
+        },
+        "Calzones": {
+            "Classic Calzone": 12.99,
+            "Meatball Calzone": 13.99,
+            "Spinach Ricotta Calzone": 13.99,
+            "Vegetable Calzone": 12.99
+        },
+        "Sides": {
+            "Garlic Breadsticks": 6.99,
+            "Cheese Sticks": 7.99,
+            "Chicken Wings": 8.99,
+            "Caesar Salad": 8.99
+        },
+        "Desserts": {
+            "Tiramisu": 6.99,
+            "Cannoli": 5.99,
+            "Chocolate Lava Cake": 7.99,
+            "Gelato": 5.99
+        }
+    },
+    
+    "Italian": {
+        "Antipasti": {
+            "Bruschetta": 8.99,
+            "Caprese Salad": 9.99,
+            "Fried Calamari": 12.99,
+            "Stuffed Mushrooms": 10.99
+        },
+        "Pasta": {
+            "Spaghetti Carbonara": 14.99,
+            "Fettuccine Alfredo": 13.99,
+            "Lasagna": 15.99,
+            "Penne Arrabiata": 12.99
+        },
+        "Pizza": {
+            "Margherita": 12.99,
+            "Pepperoni": 13.99,
+            "Quattro Formaggi": 14.99,
+            "Prosciutto Arugula": 15.99
+        },
+        "Risotto": {
+            "Mushroom Risotto": 14.99,
+            "Seafood Risotto": 16.99,
+            "Asparagus Risotto": 15.99,
+            "Saffron Risotto": 13.99
+        },
+        "Main Courses": {
+            "Chicken Parmigiana": 16.99,
+            "Veal Marsala": 18.99,
+            "Eggplant Parmigiana": 14.99,
+            "Beef Tenderloin": 21.99
+        },
+        "Desserts": {
+            "Tiramisu": 8.99,
+            "Cannoli": 6.99,
+            "Panna Cotta": 7.99,
+            "Gelato": 5.99
+        }
     }
 }
