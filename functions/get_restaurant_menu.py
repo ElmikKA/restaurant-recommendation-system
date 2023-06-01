8e5dc92f1786b8d881ef334aabf4ecc6258d55fe
elmik <elmik.karl@gmail.com>
2023-06-01 14:17:39 +0300

    added restaurant logos and a function for logos

--- functions/get_restaurant_menu.py
+++ functions/get_restaurant_menu.py
@@ -1,9 +1,11 @@
 from data.restaurant_menus import restaurant_menus
 #If the import is from the same folder then you have to but "." infront of the file name 
 from .goodbye_script import goodbye_script
+from data.restaurant_logos import restaurant_logos
 
 def restauran_function(restaurant_type): 
     
+    show_restaurants_logo(restaurant_type)
     show_restaurant_menu(restaurant_type)
     user_input_for_yes_or_no = input("Do you see anything you would like to eat? Please type y/n: ")
     food_picking(restaurant_type,  user_input_for_yes_or_no)
@@ -11,6 +13,7 @@ def restauran_function(restaurant_type):
 
 def show_restaurant_menu(restaurant_type): 
 
+    print("Hello and welcome to {0} restaurant. Please look at the menu and let us know when you are ready to order.".format(restaurant_type))
     if restaurant_type in restaurant_menus.keys():
         print("=====================================")
         for category, items in restaurant_menus[restaurant_type].items(): 
@@ -52,3 +55,9 @@ def food_picking(restaurant_type, user_input_for_yes_or_no, price = 0,):
 
     return price
 
+def show_restaurants_logo(restaurant_type):
+
+    for restaurant_logo in restaurant_logos.keys(): 
+        if restaurant_type in restaurant_logo:
+            logo = restaurant_logos[restaurant_type]
+            print(logo)            
